65aa1706d96a1515aeac10c41f5408f8c9365163
MA0035 <muzaffarashurov1982@gmail.com>
2025-09-04 12:28:08 +0500

    create project

---
+++ src/main/java/com/cafemanager/CafeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cafemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafemanager/CafeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
